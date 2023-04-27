5c4c4975b68a69f9c3344d89f90654e9b0e378de
Laureano Gasser <89240356+Memitoo@users.noreply.github.com>
2023-04-28 01:58:19 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function convertir() {
+    let pesos = document.getElementById("pesos").value;
+    let dolares = pesos / 500;
+
+    if(pesos === ""){
+        swal("Formulario Incompleto","Ingrese una cantidad mayor a 0","error")
+    }else if (pesos <= 0){
+        swal("Formulario Erróneo", "Ingrese una cantidad mayor a 0","error")
+    }else{
+        (document.getElementById("dolares")).value = dolares;
+    }
+} 
+
+function enviarForm(){
+    swal("Correcto","Formulario enviado","success");
+}
